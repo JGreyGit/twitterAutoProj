ad7234235a11526c9e0a7fb284647409241382ec
J <josephgreyconsulting@gmail.com>
2024-07-02 22:58:07 +0100

    updated quotes to have hashtags 2

---
+++ cypress/e2e/downlaodPhoto/downloadPhotoFromTwitterPlugin.cy.js
@@ -0,0 +1,6 @@
+//login to twitter
+//search for e.g. #funny dog 
+//ensure plugin exists 
+//clck on photo then when open , click on download icon
+//then store each photo into the code in a foldr called dogPhotoBucket.
+//code should also check if picture id number name already exists, if exists then dont store 
